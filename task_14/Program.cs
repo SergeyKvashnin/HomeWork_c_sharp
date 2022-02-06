bd4da55732316ec7d8f4b053f1749daeeada9b2b
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-06 11:48:26 +0300

    add task_14

---
+++ task_14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Clear(); 
+Console.WriteLine("введите чилсло: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("третьего числа нет");
+    return;
+}
+while (num > 999)
+{
+    num = num / 10;  
+}
+if (num < 1000)
+{
+    int e = num % 10;
+    Console.WriteLine("третье число " + e);
+}
