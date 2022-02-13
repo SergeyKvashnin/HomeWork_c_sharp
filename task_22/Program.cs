d94693772d0ee7c7e654e81f6faeb4288fea078c
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-13 15:05:41 +0300

    add task_22 убрал лишнее

--- task_22/Program.cs
+++ task_22/Program.cs
@@ -1,17 +1,6 @@
 ﻿//
 Console.Clear();
-    //double x = 2, y = 8;
-
-    //double result = Math.Pow(x, y);
-
-    //Console.WriteLine($"{x} ^ {y} = {result}");
-
-    //double x = 256;
-
-    //double result = Math.Sqrt(x);
-
-    //Console.WriteLine($"Квадратный корень {x} равен: {result}");
-
+    
 Console.WriteLine("Введите координату х1: ");
 double x1 = double.Parse(Console.ReadLine());
     //Console.WriteLine(x1);
@@ -30,12 +19,7 @@ double i = 2;       // степень
 double A = Math.Pow((x2-x1), i);
 double B = Math.Pow((y2-y1), i);
 double D = Math.Pow((z2-z1), i);
-//double B = y2-y1;
 
-//double n = Math.Pow(x, y);
-
-//double result = Math.Sqrt(x);
-//Console.WriteLine(A +"точка а" +  B);
 double C = Math.Sqrt(A+B+D);
 
 Console.WriteLine("расстояние между 2х точек: " + C);
