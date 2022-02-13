c006c8679a1eb16884be898f45fc1a185aca897a
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-13 17:44:58 +0300

    add task_32

---
+++ task_32/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int [] array = new int [8];    
+for (int i = 0; i < 8; i++)             // i - индекс     
+{
+    array[i] =  new Random().Next(0, 2);
+    Console.Write(array[i] + "\t");
+}
+
