571f68403c420ba3961a72dc7639fe3551bc44ef
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-15 21:11:11 +0300

    add task_28

---
+++ task_28/Program.cs
@@ -0,0 +1,13 @@
+﻿// посчитать сумму чисел числа 
+
+// Подсчитайте сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("введите число: "); 
+int numA = int.Parse(Console.ReadLine());
+int summ = 0;
+while (numA>0)
+{
+    summ=summ+numA%10;
+    numA=numA/10;
+}
+Console.WriteLine(summ);
