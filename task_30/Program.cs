400daa8b2ad83ac193e858a96bc85c9a3937306b
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-15 21:08:04 +0300

    add task_30

---
+++ task_30/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать кубы чисел заканчивающихся на четную цифру 
+
+
+int [] mass = new int [8];    
+for (int i = 0; i < 8; i++)              
+{
+    mass[i] =  new Random().Next(1, 9);
+    Console.Write(" " + mass[i]);
+    
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < mass.Length; i++)
+{
+    if(mass[i]%2 == 0)
+    {
+        Console.Write(Math.Pow(mass[i], 3)  + " ");
+    }
+}
+
