4cc3ff88ebfed4bb9dee2c6cd9a89e5d097594b1
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-15 21:10:12 +0300

    add task_26

---
+++ task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("введите число А: ");
+int numA = int.Parse(Console.ReadLine());           
+Console.WriteLine("введите степень числа: ");
+int numB = int.Parse(Console.ReadLine());           
+
+int result = 1;
+
+for (int count = 1; count<=numB; count++) 
+{
+    result = numA * result;
+}
+Console.WriteLine(result);   
+    
+
+ 
