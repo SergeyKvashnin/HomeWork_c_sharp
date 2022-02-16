6249d1bb0fb7152ee366118538334c66bcb6345d
Sergey Kvashnin <skvashnin1992@yandex.ru>
2022-02-16 22:23:01 +0300

    add task_38

---
+++ task_38/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+Console.Clear();
+int[] mass = new int[10];
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(1, 9);
+    Console.Write(mass[i] + " ");
+}
+Console.WriteLine();
+
+int summ =0;
+for (int i = 0; i < mass.Length; i++)
+{
+    if ( i%2==1)
+    {
+        summ = summ + mass[i];
+    }
+}
+
+Console.WriteLine("Сумма чисел стоящих на нечетной позиции: " + summ);
+
+
